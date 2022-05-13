5f6ce0d22e1b137a9a2a9c20b50a00b9f4d34c1a
Gordon Moore <gjmoore13@gmail.com>
2022-05-13 17:31:32 -0500

    initial commit of rrScraper (I cant stand light mode)

---
+++ rrScraper.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = "https://www.royalroad.com/fiction/22518/chrysalis/chapter/331662/the-furious-tiny-the-delectable-feast"
+page = requests.get(url)
+cont = str(page.content)
+
+soup = bs(page.content, 'html.parser')
+#results = soup.find_all("div", class_="chapter-inner chapter-content")
+results = soup.find_all("span", attrs={"style":"font-size: 1.3em"})
+
+a = open('holder.txt', 'w')
+for result in results:
+    print(result.text.strip())
+    a.write(result.text.strip() + '\n')
+
+
+
+a.close()
