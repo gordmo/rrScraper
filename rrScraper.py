b78d3fc4b4e402689eb9239eaa03c5e270394fe7
Gordon Moore <gjmoore13@gmail.com>
2022-06-15 08:17:06 -0500

    cleared holder, added documentation for futher work

--- rrScraper.py
+++ rrScraper.py
@@ -12,7 +12,10 @@ a = open('holder.txt', 'w')
 for result in results:
     print(result.text.strip())
     a.write(result.text.strip() + '\n')
-
-
-
 a.close()
+
+#now that we have the text, we can loop through all of the pages to compile the whole story
+#we can do this by adding the chapter number to the end of the url
+#we can then use the same code to get the text
+
+
