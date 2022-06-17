e18a3392eb8ad68a1d09d7097f1526c50479aafe
Gordon Moore <gjmoore13@gmail.com>
2022-06-17 18:56:28 -0500

    pushing before pc reset to be sure

--- rrScraper.py
+++ rrScraper.py
@@ -1,17 +1,30 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = "https://www.royalroad.com/fiction/22518/chrysalis/chapter/331662/the-furious-tiny-the-delectable-feast"
+import ebooklib
+from ebooklib import epub
+
+book = epub.EpubBook()
+
+#set book metadeta
+book.set_identifier('id123456')
+book.set_title('Sample book')
+book.set_language('en')
+
+book.add_author('Author')
+
+url = "https://www.royalroad.com/fiction/22518/chrysalis"
 page = requests.get(url)
 cont = str(page.content)
 
 soup = bs(page.content, 'html.parser')
-results = soup.find_all("span", attrs={"style":"font-size: 1.3em"})
-
+results = soup.find_all(class_ = "text-right")
 a = open('holder.txt', 'w')
 for result in results:
-    print(result.text.strip())
-    a.write(result.text.strip() + '\n')
+    #link = row.find('a').get('href')
+    print(result.get('href'))
+    #print(result.text) #.text.strip()
+    #a.write(result.text + '\n') #.text.strip()
 a.close()
 
 #now that we have the text, we can loop through all of the pages to compile the whole story
@@ -19,3 +32,12 @@ a.close()
 #we can then use the same code to get the text
 
 
+
+#soup = bs(page.content, 'html.parser')
+#results = soup.find_all("span", attrs={"style":"font-size: 1.3em"})
+
+#a = open('holder.txt', 'w')
+#for result in results:
+    #print(result.text.strip())
+    #a.write(result.text.strip() + '\n')
+#a.close()
