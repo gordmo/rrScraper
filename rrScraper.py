5e5213d0b2fb37fc905e7aaf58b92ef2ed9a58c7
Gordon Moore <gjmoore13@gmail.com>
2022-06-22 09:59:10 -0500

    finish planning epub building using documentation

--- rrScraper.py
+++ rrScraper.py
@@ -6,13 +6,13 @@ from ebooklib import epub
 
 book = epub.EpubBook()
 
-#set book metadeta
+#set book metadeta (will parse for author and other metadata later)
 book.set_identifier('id123456')
 book.set_title('Sample book')
 book.set_language('en')
-
 book.add_author('Author')
 
+#grabbing page full of chapters
 url = "https://www.royalroad.com/fiction/22518/chrysalis"
 page = requests.get(url)
 cont = str(page.content)
@@ -27,9 +27,7 @@ for result in results:
     #a.write(result.text + '\n') #.text.strip()
 a.close()
 
-#now that we have the text, we can loop through all of the pages to compile the whole story
-#we can do this by adding the chapter number to the end of the url
-#we can then use the same code to get the text
+
 
 
 
@@ -41,3 +39,44 @@ a.close()
     #print(result.text.strip())
     #a.write(result.text.strip() + '\n')
 #a.close()
+
+#create content before adding to book
+c1 = epub.EpubHtml(title='introTest',
+                    file_name='introTest.xhtml',
+                    lang='en')
+c1.set_content(u'<html><body><h1>Introduction</h1><p>Introduction paragraph.</p></body></html>')
+
+
+c2 = epub.EpubHtml(title='About this book',
+                   file_name='about.xhtml')
+c2.set_content('<h1>About this book</h1><p>This is a book.</p>')
+
+#adding content to the book
+book.add_item(c1)
+book.add_item(c2)
+#technically this could hold any number of things (style sheets, images, html)
+
+style = 'body { font-family: Times, Times New Roman, serif; }'
+nav_css = epub.EpubItem(uid="style_nav",
+                        file_name="style/nav.css",
+                        media_type="text/css",
+                        content=style)
+book.add_item(nav_css)
+
+#table of contents
+book.toc = (epub.Link("introTest.xhtml", "introTest", 'intro'),
+            (
+                epub.Section("Languages"),
+                (c1, c2)
+            )
+)
+
+#creating the spine
+book.spine = ['nav', c1, c2]
+
+#add NCX and nav
+book.add_item(epub.EpubNcx())
+book.add_item(epub.EpubNav())
+
+#writing the book
+epub.write_epub('test.epub', book, {})
